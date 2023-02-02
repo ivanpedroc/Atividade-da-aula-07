c77c4c5a96ffc3ccbd19a1eae17bbd4f8123be87
ivanpedroc <122410036+ivanpedroc@users.noreply.github.com>
2023-02-02 09:49:50 -0300

    Atividade aula 7
    Calcular potência

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-função  potência ( numero ,  expoente ) {
-    deixe  resultado = numero ;
-    if  ( expoente < 0 ) {
-    for ( let  i = expoente ;  i < - 1 ; i ++ ) {
-        resultado *= número ;
+1
+function potencia(numero, expoente){
+2
+    if (numero === 0){
+3
+        return "Não definido";
+4
     }
+5
+    if (expoente === 0){
+6
+        return 1;
+7
     }
-    outra coisa {
-    for ( deixe  i = 1 ;  i < expoente ; i ++ ) {
-        resultado  *= número ;
-    }
-    }
-    if  ( expoente === 0  &&  numero === 0 ) {
-        return  "não definido" ;
-    }
-    else  if  ( expoente === 0  &&  numero !== 0 ) {
-        retornar  1 ;
-    }
-    else  if  ( expoente < 0 ) {
-        return  "1/" + resultado ;
-    }
+8
 
+9
+    let resultado = numero;
+10
 
-     resultado de retorno ;
+11
+    if (expoente < 0){
+12
+        for(let i = 1; i < (-expoente); i++){
+13
+            resultado *= numero;
+14
+        }
+15
+     return 1/resultado;
+16
+    }
+17
+ 
+18
+    for(let i = 1; i < expoente; i ++){
+19
+        resultado *= numero;
+20
+    }
+21
+    return resultado;
+22
 }
-alerta ( potência ( 3 , - 2 ) ) ;
+23
 
+24
+alert(potencia(5, -3));
